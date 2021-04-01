856533ad1163563b1b766f31e99fd03b40a7e48d
Emmett Kotlikoff <ekotlikoff@gmail.com>
2021-03-31 22:19:26 -0400

    TravisCI build fix, install protoc

--- Makefile
+++ Makefile
@@ -1,19 +1,22 @@
-all: buildrpc buildweb
+all: web
 	go install ./...
 	go test ./...
 
 webclient_package := github.com/Ekotlikoff/gochess/internal/client/web
 run_local_package := github.com/Ekotlikoff/gochess/cmd/webserver
 
-buildweb:
+web:
 	GOARCH=wasm GOOS=js go build \
 		   -o $(GOPATH)/src/gochess/internal/server/frontend/static/lib.wasm \
 		   -tags webclient $(webclient_package)
 
-buildrpc:
+proto:
 	protoc --go_out=. --go_opt=paths=source_relative \
     --go-grpc_out=. --go-grpc_opt=paths=source_relative \
     api/chessengine.proto
+
+testrace:
+	go test -race -cpu 1,4 -timeout 7m ./...
 
 runweb: buildweb
 	go run $(run_local_package)
---
+++ vet.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -ex  # Exit on error; debugging enabled.
+set -o pipefail  # Fail a pipe if any sub-command fails.
+
+# not makes sure the command passed to it does not exit with a return code of 0.
+not() {
+  # This is required instead of the earlier (! $COMMAND) because subshells and
+  # pipefail don't work the same on Darwin as in Linux.
+  ! "$@"
+}
+
+fail_on_output() {
+  tee /dev/stderr | not read
+}
+
+PROTOBUF_VERSION=3.14.0
+PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
+pushd /home/travis
+wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
+unzip ${PROTOC_FILENAME}
+bin/protoc --version
+popd
+
+# - gofmt, goimports, golint (with exceptions for generated code), go vet.
+#gofmt -s -d -l . 2>&1 | fail_on_output
+#goimports -l . 2>&1 | not grep -vE "\.pb\.go"
+#golint ./... 2>&1 | not grep -vE "/testv3\.pb\.go:"
+#go vet -all ./...
+
+
+PATH="/home/travis/bin:${PATH}" make proto && \
+    git status --porcelain 2>&1 | fail_on_output || \
+    (git status; git --no-pager diff; exit 1)
