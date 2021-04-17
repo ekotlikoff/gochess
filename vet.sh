67adf3a4025716180715c622fcd8308ca556ec01
Emmett Kotlikoff <ekotlikoff@gmail.com>
2021-04-16 23:40:19 -0400

    Update vet to run staticcheck for go 1.16

--- vet.sh
+++ vet.sh
@@ -80,6 +80,6 @@ for MOD_FILE in $(find . -name 'go.mod'); do
 done
 
 SC_OUT="$(mktemp)"
-staticcheck -go 1.9 -checks 'inherit,-ST1015' ./... > "${SC_OUT}" || true
+staticcheck -go 1.16 -checks 'inherit,-ST1015' ./... > "${SC_OUT}" || true
 # Error if anything other than deprecation warnings are printed.
 not grep -v "is deprecated:.*SA1019" "${SC_OUT}"
